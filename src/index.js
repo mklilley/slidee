ae2b72022ee894222e4a39cefd0b81bb64356529
Matt Lilley <m.k.lilley@gmail.com>
2022-08-31 08:31:36 +0100

    Adds speaker notes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/moon.css";
 import "reveal.js/plugin/highlight/monokai.css";
 
 let deck = new Reveal({
-    plugins: [Markdown, RevealHighlight],
+    plugins: [Markdown, RevealHighlight, RevealNotes],
 });
 deck.initialize();
