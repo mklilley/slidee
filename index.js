d30300cf337dea1068379a75c3fa4311abdf25a3
Matt Lilley <m.k.lilley@gmail.com>
2022-09-05 10:04:10 +0100

    Creates clickable link in terminal to load slidee server

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log(`Server listening on port ${port}`));
+app.listen(3000, () =>
+    console.log(`Slidee server ready at  http://127.0.0.1:${port}`)
+);
