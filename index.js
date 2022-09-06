22e6564ae7179687a14c3344f865b4604b458ecc
Matt Lilley <m.k.lilley@gmail.com>
2022-09-06 14:12:26 +0100

    Allows slidee to open browser when it loads

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #! /usr/bin/env node
 
+const open = require("open");
+
 const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () =>
-    console.log(`Slidee server ready at  http://127.0.0.1:${port}`)
-);
+app.listen(3000, async () => {
+    console.log(`Slidee server ready at  http://127.0.0.1:${port}`);
+    await open(`http://127.0.0.1:${port}`);
+});
