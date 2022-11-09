d6b15c8e8d257ad8fd31ee5136038a7b175adf98
Matt Lilley <m.k.lilley@gmail.com>
2022-11-09 11:51:05 +0000

    Fix deck router bug where index.html will be served before deck index route

--- routes/decks.js
+++ routes/decks.js
@@ -3,12 +3,12 @@ const express = require("express");
 
 const decksController = require("../controllers/decks");
 
+router.get("/", decksController.index);
+
 // This express.static middleware allows local images to be used in the markdown files.
 // The route directory that the files are served from is the directory that slidee is 
 // launched from.
 router.use(express.static("./"));
-
-router.get("/", decksController.index);
 
 // This catch all route is required in order to serve slide decks in a way that
 // mirrors the file/folder structure on the users computer.
